efda203f18df49a96f72edf17f1578a47bd34dbf
harsh_sharma <ha201001039@gmail.com>
2013-07-15 14:11:31 +0530

    example_app

--- example_app.cpp
+++ example_app.cpp
@@ -2,12 +2,15 @@
  * Simple Wiselib Example
  */
 #include "/home/mindfuck/wiselib/wiselib.testing/external_interface/pc/pc_os_model.h"
+#include "/home/mindfuck/wiselib/wiselib.testing/external_interface/pc/pc_wiselib_application.h"
 #include "/home/mindfuck/wiselib/wiselib.stable/external_interface/external_interface.h"
 //#include "algorithms/routing/tree/tree_routing.h"
 #include "/home/mindfuck/SD_Wiselib/sd_filesys_lib.h"
 
 typedef wiselib::PCOsModel Os;
-
+typedef Os::AppMainParameter app;
+typedef typename Os::block_data_t block_data_t;
+block_data_t buffer[512];
 class ExampleApplication
 {
    public:
@@ -19,6 +22,8 @@ class ExampleApplication
 		//typedef wiselib::ArduinoOsModel Os;
 		wiselib::SdFileSystemLibrary<Os> f;
 		f.init();
+		wiselib::File<Os> x=f.open("abc.txt");
+		debug_->debug("%d",x.read(buffer,512));
      }
       // --------------------------------------------------------------------
    private:
@@ -31,8 +36,4 @@ void application_main( Os::AppMainParameter& value )
 {
  example_app.init( value );
 }
-int main()
-{
-application_main(Os);
-return 0;
-}
+
